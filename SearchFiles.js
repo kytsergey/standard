0fcae33a04ce914b2091979513ebdb359baa81e1
Sergey Kutsenko <35540778+kytsergey@users.noreply.github.com>
2018-01-21 20:57:09 +0200

    SearchFiles.js
    The application searches for all files that contains a specified string. 
    Written by JavaScript programming language.

---
+++ SearchFiles.js
@@ -0,0 +1,9 @@
+function SearchFilesName (file)
+{
+      if (file == files)
+      {    
+          console.log( " File Names :" +file);
+          return file
+      }
+ }     
+      varchar file = " "  
